da9e11c11998b37d9a5ca4ceef3bb1966074249f
DownIsOnlyWay <54189254+DownIsOnlyWay@users.noreply.github.com>
2022-05-10 14:31:51 +0300

    first try to deploy code

---
+++ src/main/java/com/example/doctorappdemo/entity/User.java
@@ -0,0 +1,74 @@
+package com.example.doctorappdemo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "data")
+    private String data;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String pass;
+
+    public User() {
+    }
+
+    public User(String data, String userName, String email, String pass) {
+        this.data = data;
+        this.userName = userName;
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String username) {
+        this.userName = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
